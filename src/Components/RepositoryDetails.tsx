a46e1d91b8cbe12c97d45317143a07855f4b51b4
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-11-08 16:36:27 -0300

    Listado de repositorios funcionando

--- src/Components/RepositoryDetails.tsx
+++ src/Components/RepositoryDetails.tsx
@@ -4,28 +4,39 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { useAuth } from '../Auth/AuthContext'
 import axios from 'axios'
 
-interface RepositoryDetailsProps {
-  // Agrega cualquier prop que necesites para el componente
+interface Repository {
+  _id: string
   search: string
-  user: string
-  description: string
-  language: string
+  user: {
+    username: string
+  }
+  reposlist: [
+    {
+      name: string
+      user: string
+      description: string
+      language: string
+      url: string
+      created_at: string
+      pushed_at: string
+    }
+  ]
   comment: string
-  url: string
   createdAt: string
   updatedAt: string
 }
 
-const RepositoryDetails: React.FC<RepositoryDetailsProps> = () => {
+const RepositoryDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>()
   const navigate = useNavigate()
   const { state } = useAuth()
-  const [repositoryData, setRepositoryData] = useState<any>({})
+  const [repositoryData, setRepositoryData] = useState<Repository | null>(null)
+  const [detailsVisible, setDetailsVisible] = useState(false)
+  const [newComment, setNewComment] = useState('') // Nuevo estado para el comentario
 
   useEffect(() => {
     const fetchRepositoryData = async () => {
       try {
-        // Realiza una solicitud al backend para obtener los detalles del repositorio por su ID
         const response = await axios.get(
           `http://localhost:3000/searchrepos/${id}`,
           {
@@ -45,43 +56,114 @@ const RepositoryDetails: React.FC<RepositoryDetailsProps> = () => {
 
   const handleDeleteRepository = async () => {
     try {
-      // Realiza una solicitud al backend para eliminar el repositorio por su ID
       await axios.delete(`http://localhost:3000/searchrepos/${id}`, {
         headers: {
           Authorization: `${state.token}`
         }
       })
-      navigate(`/user/${state.username}/reposlistbd`) // Redirige a la lista de repositorios después de eliminar
+      navigate(`/user/${state.username}/reposlistbd`)
     } catch (error) {
       console.error('Error al eliminar el repositorio', error)
+    }
+  }
+
+  const handleAddOrUpdateComment = async () => {
+    try {
+      const currentDate = new Date() // Obtiene la fecha actual
+
+      const response = await axios.patch(
+        `http://localhost:3000/searchrepos/${id}`,
+        { comment: newComment, updatedAt: currentDate.toISOString() },
+        {
+          headers: {
+            Authorization: `${state.token}`
+          }
+        }
+      )
+      // Actualiza el comentario y la fecha en el estado
+      if (repositoryData) {
+        const updatedRepositoryData = {
+          ...repositoryData,
+          comment: newComment,
+          updatedAt: currentDate.toISOString()
+        }
+        setRepositoryData(updatedRepositoryData)
+      }
+      setNewComment('') // Limpia el campo de entrada
+      console.log('Comentario actualizado:', response.data)
+    } catch (error) {
+      console.error('Error al agregar o actualizar el comentario', error)
     }
   }
 
   return (
     <div>
       <h1 className="text-light">Detalles del Repositorio Buscado</h1>
-      <div className="text-light">
-        <h2>Nombre del repositorio: {repositoryData.search}</h2>
-        <p>Usuario: {repositoryData.user?.username}</p>
-        <p>Descripción: {repositoryData.description}</p>
-        <p>Tecnología: {repositoryData.language}</p>
-        <p>
-          Fecha de creación:{' '}
-          {repositoryData.createdAt
-            ? format(new Date(repositoryData.createdAt), 'dd/MM/yyyy HH:mm')
-            : 'Fecha no disponible'}
-        </p>
-        <p>
-          Última actualización:{' '}
-          {repositoryData.updatedAt
-            ? format(new Date(repositoryData.updatedAt), 'dd/MM/yyyy HH:mm')
-            : 'Fecha no disponible'}
-        </p>
-        <p>URL: {repositoryData.url}</p>
-        <p>Comentario: {repositoryData.comment}</p>
-        {/* Agrega más detalles del repositorio según tu modelo de datos */}
-      </div>
-      <button onClick={handleDeleteRepository}>Eliminar Repositorio</button>
+      {repositoryData ? (
+        <div className="text-light">
+          <h2>Nombre del repositorio: {repositoryData.search}</h2>
+          <p>Usuario: {repositoryData.user.username}</p>
+          <p>
+            Fecha de creación:{' '}
+            {repositoryData.createdAt
+              ? format(new Date(repositoryData.createdAt), 'dd/MM/yyyy HH:mm')
+              : 'Fecha no disponible'}
+          </p>
+          <p>
+            Última actualización:{' '}
+            {repositoryData.updatedAt
+              ? format(new Date(repositoryData.updatedAt), 'dd/MM/yyyy HH:mm')
+              : 'Fecha no disponible'}
+          </p>
+          <p>Comentario: {repositoryData.comment}</p>
+          {/* Campo de entrada para el comentario */}
+          <input
+            type="text"
+            placeholder="Comentar"
+            value={newComment}
+            onChange={e => setNewComment(e.target.value)}
+          />
+          {/* Botón para agregar o editar el comentario */}
+          <button className="mb-3" onClick={handleAddOrUpdateComment}>
+            Agregar/Editar Comentario
+          </button>
+          <br />
+          <button onClick={() => setDetailsVisible(!detailsVisible)}>
+            {detailsVisible ? 'Ocultar detalles' : 'Mostrar detalles'}
+          </button>
+          <button onClick={handleDeleteRepository}>Eliminar Repositorio</button>
+          {detailsVisible ? (
+            <div>
+              <h4 className="mt-3">Detalles adicionales:</h4>
+              {repositoryData.reposlist.map((repo, index) => (
+                <div key={index} className="mt-3">
+                  <h5>Repositorio {index + 1}:</h5>
+                  <p className="mt-3">Nombre: {repo.name}</p>
+                  <p>Usuario: {repo.user}</p>
+                  <p>Descripción: {repo.description}</p>
+                  <p>Lenguaje: {repo.language}</p>
+                  <p>
+                    Fecha de creación:{' '}
+                    {repo.created_at
+                      ? format(new Date(repo.created_at), 'dd/MM/yyyy HH:mm')
+                      : 'Fecha no disponible'}
+                  </p>
+                  <p>
+                    Última actualización:{' '}
+                    {repo.pushed_at
+                      ? format(new Date(repo.pushed_at), 'dd/MM/yyyy HH:mm')
+                      : 'Fecha no disponible'}
+                  </p>
+                  <p className="mb-4">URL: {repo.url}</p>
+                  <div className="line-h2 mb-4"></div>
+                </div>
+              ))}
+            </div>
+          ) : null}
+        </div>
+      ) : (
+        <p>Cargando detalles del repositorio...</p>
+      )}
     </div>
   )
 }
